9287bae2890ced21a65e8bd05f266e139d322876
Cornerstone Ephraim <fortunecornerstone@gmail.com>
2022-06-24 02:13:12 +0100

    added custom scrollbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
     <div className="header">
       <div className="mobile">
         <h1 className="logo" onClick={() => navigate("/")}>
-          The Logo
+          Logo
         </h1>
         <div className="menu">
           <img src={require("../assets/icons/hamburger.svg").default} alt=""/>
@@ -16,7 +16,7 @@ const Header = () => {
       </div>
       <div className="desktop">
         <h1 className="logo" onClick={() => navigate("/")}>
-          The Logo
+          Logo
         </h1>
         <nav className="navigation">
           <div className="links">
